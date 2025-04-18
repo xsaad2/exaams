f27be5c9914ff02ea2929062fe7df75fc7510270
saad belkhou <saad.belkhou@gmail.com>
2025-04-18 15:37:25 +0200

    Feat(Exams-Overview): create exam card (EX-22)

--- libs/exaams/exaam/ui/src/lib/exam-card/exam-card.component.ts
+++ libs/exaams/exaam/ui/src/lib/exam-card/exam-card.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ExamCatalogItem } from '@com.language.exams/exaams-backend/utils';
+import {
+  AtomicButtonComponent,
+  AtomicIconComponent,
+  AtomicProgressBarComponent,
+} from '@com.language.exams/shared/atomic-components';
+import { Router } from '@angular/router';
+import { B1ExamStore } from '@com.language.exams/exaams/exaam/data-access';
 
 @Component({
   selector: 'lib-exam-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    AtomicButtonComponent,
+    AtomicIconComponent,
+    AtomicProgressBarComponent,
+  ],
   templateUrl: './exam-card.component.html',
 })
-export class ExamCardComponent {}
+export class ExamCardComponent {
+  private readonly router = inject(Router);
+  private readonly examStore = inject(B1ExamStore);
+  exam = input<ExamCatalogItem>();
+
+  onButtonCLick() {
+    this.examStore.loadSelectedExam(this.exam()?.id || '');
+    this.router.navigate([`/exams/b1/${this.exam()?.id}`]);
+  }
+
+  buttonLabel = computed(() => {
+    if (this.exam()?.lastScore) {
+      return 'Take again';
+    }
+    return 'New try';
+  });
+}
